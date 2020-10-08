7b156731b987bc630751284183a0538023403cb9
Maureen Joyce <mjoyce10@kent.edu>
2020-10-08 15:02:49 -0400

    passed set description and set name tests

--- ReviewsSite.Tests/AlbumTests.cs
+++ ReviewsSite.Tests/AlbumTests.cs
@@ -11,15 +11,30 @@ namespace ReviewsSite.Tests
         Album sut;
         public AlbumTests()
         {
-            sut = new Album(1);
+            sut = new Album(1, "Album name", "Album description");
         }
 
         [Fact]
-
         public void AlbumConstructor_Sets_Id_On_CourseModel()
         {
             int result = sut.Id;
             Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void AlbumConstructor_Sets_Name_On_CourseModel()
+        {
+            string result = sut.Name;
+
+            Assert.Equal("Album name", result);
+        }
+
+        [Fact]
+        public void AlbumConstructor_Sets_Description_On_CourseModel()
+        {
+            string result = sut.Description;
+
+            Assert.Equal("Album description", result);
         }
 
 
--- ReviewsSite/Models/Album.cs
+++ ReviewsSite/Models/Album.cs
@@ -8,14 +8,18 @@ namespace ReviewsSite.Models
     public class Album
     {   
         public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
         public Album()
         {
 
         }
 
-        public Album(int id)
+        public Album(int id, string name, string description)
         {
             Id = id;
+            Name = name;
+            Description = description;
         }
     }
 }
