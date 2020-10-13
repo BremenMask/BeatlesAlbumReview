892d07a4f654336d4ea1e0b1ebfe263b799aedcb
Maureen Joyce <mjoyce10@kent.edu>
2020-10-13 15:22:04 -0400

    added ability to see details and come back to index

--- ReviewsSite/Controllers/AlbumController.cs
+++ ReviewsSite/Controllers/AlbumController.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using ReviewsSite.Models;
+﻿using ReviewsSite.Models;
 using ReviewsSite.Repositories;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,13 +10,28 @@ namespace ReviewsSite.Controllers
 {
     public class AlbumController : Controller
     {
+        IRepository<Album> albumRepo;
+
+        public AlbumController(IRepository<Album> albumRepo)
+        {
+            this.albumRepo = albumRepo;
+        }
+
         public ViewResult Index()
         {
-            AlbumRepository albumRepo = new AlbumRepository();
+           //AlbumRepository albumRepo = new AlbumRepository();
 
             var model = albumRepo.GetAll();
 
             return View(model);
         }
+
+        public ViewResult Details(int id)
+        {
+            var model = albumRepo.GetById(id);
+
+            return View(model);
+        }
+
     }
 }
