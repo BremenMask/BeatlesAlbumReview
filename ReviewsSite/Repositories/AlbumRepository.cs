93d56cf4b1c2078e762adb5b4262dced9ec1fa8d
Maureen Joyce <mjoyce10@kent.edu>
2020-10-13 11:04:26 -0400

    added review and image properties. passed tests to set properties

--- ReviewsSite/Repositories/AlbumRepository.cs
+++ ReviewsSite/Repositories/AlbumRepository.cs
@@ -14,19 +14,19 @@ namespace ReviewsSite.Repositories
         {
             albumsList = new List<Album>()
             {
-                new Album(1, "Please Please Me", "1963", "Early Career"),
-                new Album(2, "Meet the Beatles", "1964", "Early Career"),
-                new Album(3, "Hard Day's Night", "1964", "Early Career"),
-                new Album(4, "Beatles for Sale", "1964", "Early Career"),
-                new Album(5, "Help!", "1965", "Mid-Career"),
-                new Album(6, "Rubber Soul", "1965", "Mid-Career"),
-                new Album(7, "Revolver", "1966", "Mid-Career"),
-                new Album(8, "Sgt Pepper's Lonely Hearts Club Band", "1967", "Late Career"),
-                new Album(9, "Magical Mystery Tour", "1967", "Late Career"),
-                new Album(10, "White Album", "1968", "Late Career"),
-                new Album(11, "Yellow Submarine", "1969", "Late Career"),
-                new Album(12, "Abbey Road", "1969", "Late Career"),
-                new Album(13, "Let It Be", "1970", "Late Career")
+                new Album(1, "Please Please Me", "1963", "Early Career", "Review", "Image"),
+                new Album(2, "Meet the Beatles", "1964", "Early Career", "Review", "Image"),
+                new Album(3, "Hard Day's Night", "1964", "Early Career", "Review", "Image"),
+                new Album(4, "Beatles for Sale", "1964", "Early Career", "Review", "Image"),
+                new Album(5, "Help!", "1965", "Mid-Career", "Review", "Image"),
+                new Album(6, "Rubber Soul", "1965", "Mid-Career", "Review", "Image"),
+                new Album(7, "Revolver", "1966", "Mid-Career", "Review", "Image"),
+                new Album(8, "Sgt Pepper's Lonely Hearts Club Band", "1967", "Late Career", "Review", "Image"),
+                new Album(9, "Magical Mystery Tour", "1967", "Late Career", "Review", "Image"),
+                new Album(10, "White Album", "1968", "Late Career", "Review", "Image"),
+                new Album(11, "Yellow Submarine", "1969", "Late Career", "Review", "Image"),
+                new Album(12, "Abbey Road", "1969", "Late Career", "Review", "Image"),
+                new Album(13, "Let It Be", "1970", "Late Career", "Review", "Image")
             };
         }
         public IEnumerable<Album> GetAll()
