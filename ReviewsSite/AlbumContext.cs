c52cba017af6b5c6526b1c4177752604da96555e
Bremen Mask <thejames0023@gmail.com>
2020-10-14 12:00:34 -0400

    created album context class, added NuGet packages

---
+++ ReviewsSite/AlbumContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReviewsSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReviewsSite
+{
+    public class AlbumContext : DbContext
+    {
+        public DbSet<Album> Albums { get; set; }
+    }
+}
