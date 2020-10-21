12d15a6c86b41ccace29794fbbdf1605998a0079
Bremen Mask <thejames0023@gmail.com>
2020-10-21 11:38:34 -0400

    review tests completed

---
+++ ReviewsSite.Tests/ReviewTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using ReviewsSite.Models;
+using Xunit;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReviewsSite.Tests
+{
+    public class ReviewTests
+    {
+        Review sut;
+
+        public ReviewTests()
+        {
+            sut = new Review(1,"Content","ReviewerName",1,"10/21/2020",1); 
+        }
+
+        [Fact]
+        public void ReviewConstructor_Sets_Id_On_ReviewModel()
+        {
+            int result = sut.Id;
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void ReviewConstructor_Sets_ReviewerName_On_ReviewModel()
+        {
+            string result = sut.ReviewerName;
+            Assert.Equal("ReviewerName", result);
+        }
+
+        [Fact]
+        public void ReviewConstructor_Sets_Rating_On_ReviewModel()
+        {
+            int result = sut.Rating;
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void ReviewConstructor_Sets_ReviewDate_On_ReviewModel()
+        {
+            string result = sut.ReviewDate;
+            Assert.Equal("10/21/2020", result);
+        }
+
+        [Fact]
+        public void ReviewConstructor_Sets_AlbumId_On_ReviewModel()
+        {
+            int result = sut.AlbumId;
+            Assert.Equal(1, result);
+        }
+
+    }
+}
--- ReviewsSite/Models/Review.cs
+++ ReviewsSite/Models/Review.cs
@@ -12,11 +12,27 @@ namespace ReviewsSite.Models
         [MaxLength(160)]
         public string Content { get; set; }
         [Required]
+        [Display(Name = "Author")]
         public string ReviewerName { get; set; }
         public int Rating { get; set; }
         public string ReviewDate { get; set; }
         [Display(Name = "Album")]
         public int AlbumId { get; set; }
         public virtual Album Album { get; set; }
+
+        public Review()
+        { 
+        
+        }
+
+        public Review(int id, string content, string reviewerName, int rating, string reviewDate, int albumId)
+        {
+            Id = id;
+            Content = content;
+            ReviewerName = reviewerName;
+            Rating = rating;
+            ReviewDate = reviewDate;
+            AlbumId = albumId;
+        }
     }
 }
